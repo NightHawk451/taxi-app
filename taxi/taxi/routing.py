083126ab415efe3afafe49299a9205515ff715f7
Micah Pearce <micah.r.pearce@gmail.com>
2020-08-02 21:13:47 -0500

    initial commit

---
+++ taxi/taxi/routing.py
@@ -0,0 +1,13 @@
+# taxi/routing.py
+
+from django.urls import path # new
+from channels.routing import ProtocolTypeRouter, URLRouter # changed
+
+from trips.consumers import TaxiConsumer
+
+
+application = ProtocolTypeRouter({
+    'websocket': URLRouter([
+        path('taxi/', TaxiConsumer),
+    ]),
+})
